fb4d1278f8891078713597409874d589bf84abd7
SONG <geun2121@gmail.com>
2023-03-29 15:39:02 +0900

    Update Position class
    - 생성과 동시에 x, y값 랜덤세팅하게 구현

--- DrawingApp/DrawingApp/Position.swift
+++ DrawingApp/DrawingApp/Position.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Position {
+    private var x : Double = 0.0
+    private var y : Double = 0.0
+    private var xLimit : Double
+    private var yLimit : Double
     
+    init(width : Double , height : Double ) {
+        self.xLimit = width
+        self.yLimit = height
+        self.x = setX()
+        self.y = setY()
+    }
+    
+    private func setX () -> Double {
+        return Double.random(in: 0.0 ... xLimit)
+    }
+    
+    private func setY () -> Double{
+        return Double.random(in: 0.0 ... yLimit)
+    }
 }
