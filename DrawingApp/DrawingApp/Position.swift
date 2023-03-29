72509d200e0f540c1beeb896af36d8c1633c4e45
SONG <geun2121@gmail.com>
2023-03-29 12:29:35 +0900

    Create project
    - 프로젝트 및 swift파일 생성

---
+++ DrawingApp/DrawingApp/Position.swift
@@ -0,0 +1,8 @@
+//
+//  Position.swift
+//  DrawingApp
+//
+//  Created by SONG on 2023/03/29.
+//
+
+import Foundation
---
+++ DrawingApp/DrawingApp/SquareViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SquareViewController.swift
+//  DrawingApp
+//
+//  Created by SONG on 2023/03/29.
+//
+
+import UIKit
+
+class SquareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
