72549a16e02676ba3709960fdc49c81ee50577e5
SONG <geun2121@gmail.com>
2023-03-29 16:03:47 +0900

    Update Alpha enum
    - case별 rawValue 세팅

--- DrawingApp/DrawingApp/Alpha.swift
+++ DrawingApp/DrawingApp/Alpha.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
-class Alpha {
-    
+enum Alpha : CGFloat {
+    case one = 0.0
+    case two = 0.11111
+    case three = 0.22222
+    case four = 0.33333
+    case five = 0.44444
+    case six = 0.55555
+    case seven = 0.66666
+    case eight = 0.77777
+    case nine = 0.88888
+    case ten = 1
 }
