53a60770b9bb2f5afe6facf93c89e1414caac4d9
SONG <geun2121@gmail.com>
2023-03-29 14:55:23 +0900

    Update ID class
    - id 프로퍼티 선언 
    - id 생성 메소드 추가

--- DrawingApp/DrawingApp/ID.swift
+++ DrawingApp/DrawingApp/ID.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 class ID {
+    private var id : String
     
+    init() {
+        self.id = ""
+    }
+    
+    func generate() -> String {
+        var name = ""
+        
+        for i in 1...11 {
+            
+            if i == 4 || i == 8 {
+                name += "-"
+                continue
+            }
+            
+            let randomOffset = UInt32.random(in: 0...25)
+            let randomUnicodeScalar = UnicodeScalar("a").value + randomOffset
+            guard let randomLetter = UnicodeScalar(randomUnicodeScalar) else {
+                return ""
+            }
+            name += String(randomLetter)
+        }
+        self.id = name
+        return name
+    }
 }
