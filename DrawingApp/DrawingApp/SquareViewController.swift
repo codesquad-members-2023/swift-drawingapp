dc35d46d567a7ede96689b8557f5690f90bd5388
SONG <geun2121@gmail.com>
2023-03-29 18:28:59 +0900

    Update SquareViewController class
    - run() 구현

--- DrawingApp/DrawingApp/SquareViewController.swift
+++ DrawingApp/DrawingApp/SquareViewController.swift
@@ -11,8 +11,19 @@ class SquareViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        run()
     }
 
+    func run() {
+        let widthLimit = self.view.bounds.width
+        let heightLimit = self.view.bounds.height
+        let randomSquareFactory = RandomSquareFactory()
+        
+        guard let newSquare = randomSquareFactory.produce(widthLimit: widthLimit, heightLimit: heightLimit) else {
+            return
+        }
+        
+        print(newSquare)
+    }
 
 }
