9da2b98ee2572a599a0e0579f4493ff7bbdbd551
Hyejeong Park <effie.thedev@gmail.com>
2023-03-29 15:26:08 +0900

    [add] Call factory method on ViewController
    - ViewController에 팩토리 메소드 호출 코드 추가
    - view를 생성할 때마다 시스템 로그 출력하도록 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
+  
+  private var views = [DRView]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let viewXRange = (0.0 ... Double(view.frame.width))
+    let viewYRange = (0.0 ... Double(view.frame.height))
+    
+    let randomFactory = RandomViewFactory(xRange: viewXRange, yRange: viewYRange)
+    makeViews(with: randomFactory, count: 4)
   }
-
+  
+  private func makeViews(with factory: ViewFactory, count: Int) {
+    let logger = Logger()
+    for i in 0..<count {
+      guard let newView = factory.makeView() else { continue }
+      views.append(newView)
+      logger.log("Rect\(i + 1) \(newView.description)")
+    }
+  }
 }
-
