ada381d1e47c8ba7589b607928b2cfc643b11cae
SONG <geun2121@gmail.com>
2023-03-30 11:50:58 +0900

    Update SquareViewController class
    - run() 에 반복횟수 기능 추가

--- DrawingApp/DrawingApp/SquareViewController.swift
+++ DrawingApp/DrawingApp/SquareViewController.swift
@@ -11,21 +11,22 @@ class SquareViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        run()
+        run(howMany: 4)
     }
 
-    func run() {
+    func run(howMany amount : Int) {
         let log = Logger()
         
         let widthLimit = self.view.bounds.width
         let heightLimit = self.view.bounds.height
         let randomSquareFactory = RandomSquareFactory()
         
-        guard let newSquare = randomSquareFactory.produce(widthLimit: widthLimit, heightLimit: heightLimit) else {
-            return
+        for _ in 1...amount{
+            guard let newSquare = randomSquareFactory.produce(widthLimit: widthLimit, heightLimit: heightLimit) else {
+                return
+            }
+            log.printLog(of: newSquare)
         }
-        
-        log.printLog(of: newSquare)
     }
 
 }
