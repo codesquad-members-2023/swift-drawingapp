31e970b540ab748bdd932e4e91a6a143f4ca043c
SONG <geun2121@gmail.com>
2023-03-30 12:28:20 +0900

    Update Logger
    - 출력포맷 변경

--- DrawingApp/DrawingApp/Logger.swift
+++ DrawingApp/DrawingApp/Logger.swift
@@ -12,6 +12,6 @@ class Logger {
     func printLog(of sqaure: Square , order : Int) {
         let log = OSLog(subsystem: "", category: "data")
 
-        os_log("SQUARE%@ : %@", log: log, type: .info, String(order) ,sqaure.description)
+        os_log("SQUARE%@ , %@", log: log, type: .info, String(order) ,sqaure.description)
     }
 }
