ee817305ef108fbc82f657a1ac48cca09c95ac2a
SONG <geun2121@gmail.com>
2023-03-31 10:52:09 +0900

    Update Square class
    - 팩토리가 추상객체를 리턴하도록 수정하려함. 
    - 기존의 Square class 를 RandomSquare로 변경 
    - Square protocol을 선언하여 채택하게 했음

--- DrawingApp/DrawingApp/Square.swift
+++ DrawingApp/DrawingApp/Square.swift
@@ -2,29 +2,15 @@
 //  Square.swift
 //  DrawingApp
 //
-//  Created by SONG on 2023/03/29.
+//  Created by SONG on 2023/03/31.
 //
 
 import Foundation
 
-class Square {
-    var id : ID
-    var size : Size
-    var position : Position
-    var backgroundColor : Color
-    var alpha : Alpha
-    
-    init(id: ID, size: Size, position: Position, backgroundColor: Color, alpha: Alpha) {
-        self.id = id
-        self.size = size
-        self.position = position
-        self.backgroundColor = backgroundColor
-        self.alpha = alpha
-    }
-}
-
-extension Square : CustomStringConvertible {
-    var description: String {
-        return "\(self.id.description) , \(self.size.description) , \(self.position.description) , \(self.backgroundColor.description) , \(self.alpha.description)"
-    }
+protocol Square {
+    var id : ID { get }
+    var size : Size { get }
+    var position : Position { get }
+    var backgroundColor : Color { get }
+    var alpha : Alpha { get }
 }
