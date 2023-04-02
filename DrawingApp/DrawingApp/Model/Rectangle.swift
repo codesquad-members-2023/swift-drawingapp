ba1ce3a1986426b9a48663d00a057d58b66059d3
demiiann <tkim0156@gmail.com>
2023-04-03 01:14:55 +0900

    feat: Rectangle 클래스 생성 및 설계
    - 과제요구사항 고려하여 작성

---
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -0,0 +1,34 @@
+//
+//  Rectangle.swift
+//  DrawingApp
+//
+//  Created by ilim on 2023/03/31.
+//
+
+import Foundation
+
+class Rectangle {
+    private var id: String
+    private var width, height: Double
+    private var xCoordinate, yCoordinate: Double
+    private var R,G,B: Int
+    private var alpha: Int
+    
+    init(id: String, width: Double, height: Double, xCoordinate: Double, yCoordinate: Double, R: Int, G: Int, B: Int, alpha: Int) {
+        self.id = id
+        self.width = width
+        self.height = height
+        self.xCoordinate = xCoordinate
+        self.yCoordinate = yCoordinate
+        self.R = R
+        self.G = G
+        self.B = B
+        self.alpha = alpha
+    }
+}
+
+extension Rectangle: CustomStringConvertible {
+    var description: String {
+        return "((\(id)), X:\(xCoordinate),Y:\(yCoordinate), W\(width), H\(height), R:\(R), G:\(G), B:\(B), Alpha: \(alpha))"
+    }
+}
