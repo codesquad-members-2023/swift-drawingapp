3cb46fc60be4787735c41456f3595125682bbbdb
demiiann <tkim0156@gmail.com>
2023-04-03 02:59:39 +0900

    feat: 사각형 정해진 횟수만큼 생성하는 메서드 구현
    - 매개변수로 받은 정수만큼 사각형을 생성
    - 생성하고 해당 사각형의 정보를 OSLog로 출력

---
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by ilim on 2023/03/27.
+//
+
+import UIKit
+import OSLog
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let numberOfRectangles = 4
+        generateRectangles(numberOfRectangle: numberOfRectangles)
+    }
+    
+    func generateRectangles(numberOfRectangle: Int) {
+        let rectangle = RectangleFactory()
+        
+        for i in 1...numberOfRectangle {
+            let xRange = 0...Double(self.view.frame.size.width - 150)
+            let yRange = 0...Double(self.view.frame.size.height - 120)
+            let xCoordinate = Double.random(in: xRange)
+            let yCoordinate = Double.random(in: yRange)
+            let randomRectangle = rectangle.makeRectangle(x: xCoordinate, y: yCoordinate)
+            let systemLog = Logger()
+            systemLog.log("Rect\(i) \(randomRectangle)")
+        }
+
+    }
+}
+
