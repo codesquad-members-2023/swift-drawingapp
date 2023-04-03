5046258b5b922d2ef03108f1c509361cca6ec12c
demiiann <tkim0156@gmail.com>
2023-04-04 00:33:42 +0900

    add: 랜덤한 사각형의 속성 클래스 파일 생성

---
+++ DrawingApp/DrawingApp/Random/RandomCoordinate.swift
@@ -0,0 +1,9 @@
+//
+//  RandomCoordinate.swift
+//  DrawingApp
+//
+//  Created by ilim on 2023/04/03.
+//
+
+import Foundation
+// 이걸 할려면 가로 세로 길이 알아야 되는데
---
+++ DrawingApp/DrawingApp/Random/RandomRGB.swift
@@ -0,0 +1,8 @@
+//
+//  RandomRGB.swift
+//  DrawingApp
+//
+//  Created by ilim on 2023/04/03.
+//
+
+import Foundation
