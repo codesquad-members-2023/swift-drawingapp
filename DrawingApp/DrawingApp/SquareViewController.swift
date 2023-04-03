c78d3e4f4f11e5a5a016a36e0ecd6074bed6f55d
SONG <geun2121@gmail.com>
2023-04-03 16:52:54 +0900

    Update SquareViewController
    - Model은 View를 모르도록, Controller에서 해당 Square view를 생성하도록 수정 
    - 뷰를 터치 하면 해당 view의 tag가 출력되도록 설정

--- DrawingApp/DrawingApp/SquareViewController.swift
+++ DrawingApp/DrawingApp/SquareViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 class SquareViewController: UIViewController {
     let log = Logger()
     var plane = Plane()
+    var views : [UIView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+                self.view.addGestureRecognizer(tapGestureRecognizer)
     }
 
     func createSquare(howMany amount : Int) -> Square?{
@@ -40,9 +44,34 @@ class SquareViewController: UIViewController {
         guard let createdSquare = self.plane.squareIncluded.last else {
             return
         }
-        let squareView = createdSquare.convertToUIView()
+        let squareView = createSquareView(from : createdSquare.manufacturing())
         
         self.view.addSubview(squareView)
-        
     }
+    
+    func createSquareView(from material : MaterialOfSquareView) -> UIView {
+        let red = material.viewColor[0]
+        let green = material.viewColor[1]
+        let blue = material.viewColor[2]
+        let alpha = material.alpha
+        
+        var newView = UIView()
+        
+        newView.frame = material.rect
+        newView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        self.views.append(newView)
+        return newView
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+            let location = sender.location(in: self.view)
+            
+            // Get the view at the tapped location using viewWithTag
+            if let selectedView = self.view.hitTest(location, with: nil), selectedView != self.view {
+                
+                // Do something with the selected view
+                print("Selected view tag: \(selectedView.tag)")
+            }
+        }
+    
 }
