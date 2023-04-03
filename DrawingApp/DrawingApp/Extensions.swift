f28548a5374a4ad3e2890d3c28d1e6c86225447f
SONG <geun2121@gmail.com>
2023-04-03 13:29:36 +0900

    Create UIView extension
    - 메소드 하나로 Square의 정보들을 View에 세팅하는 구조로 하고 싶어서 스켈레톤 코드 작성

---
+++ DrawingApp/DrawingApp/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  DrawingApp
+//
+//  Created by SONG on 2023/04/03.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setSquare(about square : Square) {
+        self.frame.size =
+        self.frame.origin =
+        self.backgroundColor =
+        self.alpha =
+    }
+}
