9b0a6ce6f8854d9cf26241bf084e1dacbd49cbe9
demiiann <tkim0156@gmail.com>
2023-04-04 19:32:21 +0900

    refactor: 기존 멤버 변수들 별도의 타입으로 분리하여 리팩토링

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -8,27 +8,23 @@
 import Foundation
 
 class Rectangle {
-    private var id: String
-    private var width, height: Double
-    private var xCoordinate, yCoordinate: Double
-    private var R,G,B: Int
-    private var alpha: Int
+    private var id: Id
+    private var size: Size
+    private var coordinate: Coordinate
+    private var color: Rgb
+    private var alpha: Alpha
     
-    init(id: String, width: Double, height: Double, xCoordinate: Double, yCoordinate: Double, R: Int, G: Int, B: Int, alpha: Int) {
+    init(id: Id, size: Size, coordinate: Coordinate, color: Rgb, alpha: Alpha) {
         self.id = id
-        self.width = width
-        self.height = height
-        self.xCoordinate = xCoordinate
-        self.yCoordinate = yCoordinate
-        self.R = R
-        self.G = G
-        self.B = B
+        self.size = size
+        self.coordinate = coordinate
+        self.color = color
         self.alpha = alpha
     }
 }
 
 extension Rectangle: CustomStringConvertible {
     var description: String {
-        return "(\(id)), X:\(xCoordinate),Y:\(yCoordinate), W\(width), H\(height), R:\(R), G:\(G), B:\(B), Alpha: \(alpha)"
+        return "\(id), \(coordinate), \(size), \(color), \(alpha)"
     }
 }
