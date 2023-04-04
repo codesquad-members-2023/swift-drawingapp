6207f7e2202405dfe013b8dbf083d884ed4cbaa7
demiiann <tkim0156@gmail.com>
2023-04-04 23:01:36 +0900

    add : Plane 파일 생성

---
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -0,0 +1,16 @@
+//
+//  Plane.swift
+//  DrawingApp
+//
+//  Created by ilim on 2023/04/04.
+//
+
+import Foundation
+
+struct Plane {
+    // 생성된 사각형 저장 변수 (배열인가?)
+    // 사각형 전체 개수 알려주는 메서드 or 계산 프로퍼티
+    // Subscript로 index를 넘기면 해당 사각형 모델을 return한다.
+    // 배열이랑 서브스크립트랑 잘 어떻게 하면 된다더라
+    // 터치 좌표를 넘기면, 해당 위치를 포함하는 사각형이 있는지 판단한다.
+}
