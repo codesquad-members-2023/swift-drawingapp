bc45b5cb56d61d3b56aa5157beb835a0030f5e12
SONG <geun2121@gmail.com>
2023-04-06 14:54:18 +0900

    Update Extension
    - 로직 변경

--- DrawingApp/DrawingApp/Extension.swift
+++ DrawingApp/DrawingApp/Extension.swift
@@ -9,7 +9,15 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    func convert() -> Color {
-        return Color(red: Int(self.ciColor.red), green: Int(self.ciColor.green), blue: Int(self.ciColor.blue))
+    func convert() -> Color? {
+        guard let components = self.cgColor.components else{
+            return nil
+        }
+    
+        let red = Int(components[0] * 255)
+        let green = Int(components[1] * 255)
+        let blue = Int(components[2] * 255)
+
+        return Color(red: red, green: green, blue: blue)
     }
 }
