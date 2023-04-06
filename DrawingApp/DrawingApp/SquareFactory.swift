b335ce82f2d357c4cc87633241c954af785afe53
SONG <geun2121@gmail.com>
2023-04-06 15:50:36 +0900

    Rename method in Factory
    - produce() 를 makeSquare() 로 변경

--- DrawingApp/DrawingApp/SquareFactory.swift
+++ DrawingApp/DrawingApp/SquareFactory.swift
@@ -9,5 +9,5 @@ import Foundation
 
 protocol SquareFactory {
     
-    func produce(widthLimit : Double, heightLimit : Double) -> Square?
+    func makeSquare(widthLimit : Double, heightLimit : Double) -> Square?
 }
