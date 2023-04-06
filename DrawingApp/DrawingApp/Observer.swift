d181a28b92b54c6c891c1e1bd63c2008c2166a19
SONG <geun2121@gmail.com>
2023-04-06 14:18:04 +0900

    Create ModelSynchronizer class
    - VIew의 변화를 Model에 동기화 시켜주기 위한 타입 선언 
    - Observer 를 채택, 값 변화 감지 후 기능하도록 작성

--- DrawingApp/DrawingApp/Observer.swift
+++ DrawingApp/DrawingApp/Observer.swift
@@ -10,3 +10,18 @@ import Foundation
 protocol Observer {
     func detectChangeOfAlpha(synchronizeTarget : Square , alpha : CGFloat)
 }
+
+class ModelSynchronizer : Observer {
+    
+    func detectChangeOfAlpha(synchronizeTarget : Square , alpha : CGFloat) {
+        let newAlpha = round(alpha * 10)
+        var square = synchronizeTarget
+        
+        for item in Alpha.allCases {
+            if Int(newAlpha) == item.rawValue {
+                square.alpha = Alpha(rawValue: item.rawValue) ?? .five
+                break
+            }
+        }
+    }
+}
