49daed5bf885ad8555904c26828cbf80c733e7e4
SONG <geun2121@gmail.com>
2023-04-07 12:11:45 +0900

    Create testCode
    - Plane class를 타겟으로 한 unitTest

---
+++ DrawingApp/PlaneTest/PlaneTest.swift
@@ -0,0 +1,25 @@
+//
+//  PlaneTest.swift
+//  PlaneTest
+//
+//  Created by SONG on 2023/04/06.
+//
+
+import XCTest
+
+final class PlaneTest: XCTestCase {
+    var test : Plane!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+    }
+
+    override func tearDownWithError() throws {
+        test = nil
+        try super.tearDownWithError()
+
+    }
+}
+
+
