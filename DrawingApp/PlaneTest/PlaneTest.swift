00d25d421f14404e089276ad6e52edc1e49c096c
SONG <geun2121@gmail.com>
2023-04-07 12:13:31 +0900

    Update PlaneTest
    - Plane과 의존관계인 구현체들의 껍데기만 선언

--- DrawingApp/PlaneTest/PlaneTest.swift
+++ DrawingApp/PlaneTest/PlaneTest.swift
@@ -22,4 +22,61 @@ final class PlaneTest: XCTestCase {
     }
 }
 
+protocol Square {
+    var id : ID { get }
+    var size : Size { get }
+    var position : Position { get }
+    var backgroundColor : Color { get set }
+    var alpha : Alpha { get set}
+    var description : String { get }
+    
+    func manufacturing() -> MaterialOfSquareView
+}
+
+struct ID {
+    
+}
+
+struct Size{
+    
+}
+
+struct Position {
+    let x = 0.0
+    let y = 0.0
+    
+    func isCoordinateContained(point: CGPoint)-> Bool {
+        let x = Double(point.x)
+        let y = Double(point.y)
+        let xRange = self.x...self.x + 150 // 150 = width
+        let yRange = self.y...self.y + 120 // 120 = height
+        
+        return xRange.contains(x) && yRange.contains(y)
+    }
+}
+
+struct Color {
+    
+}
+
+struct Alpha{
+    
+}
+
+struct MaterialOfSquareView {
+    
+}
+
+class MockSquare : Square{
+    var id = ID()
+    var size = Size()
+    var position = Position()
+    var backgroundColor = Color()
+    var alpha = Alpha()
+    var description: String = ""
+    
+    func manufacturing() -> MaterialOfSquareView{
+        return MaterialOfSquareView()
+    }
+}
 
