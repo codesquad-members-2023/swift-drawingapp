bca5af9dfb83901b230a48f23d911187bf13a432
SONG <geun2121@gmail.com>
2023-04-13 14:06:00 +0900

    Update Extension
    - UIColor를 customeType으로 변경하는 익스텐션

--- DrawingApp/DrawingApp/Extension.swift
+++ DrawingApp/DrawingApp/Extension.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    func convert() -> Color? {
+    func colorConvert() -> Color? {
         guard let components = self.cgColor.components else{
             return nil
         }
@@ -19,5 +19,21 @@ extension UIColor {
         let blue = Int(components[2] * 255)
 
         return Color(red: red, green: green, blue: blue)
+    }
+    
+    func alphaConvert() -> Alpha? {
+        
+        guard let components = self.cgColor.components else{
+            return nil
+        }
+        
+        let value = Int((components[3] * 10).rounded())
+        
+        for item in Alpha.allCases {
+            if value == item.rawValue {
+                return item
+            }
+        }
+        return nil
     }
 }
