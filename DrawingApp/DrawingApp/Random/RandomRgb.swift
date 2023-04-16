b672bc17b49bf1976145e05370fa209ff3b0f092
demiiann <tkim0156@gmail.com>
2023-04-16 21:19:10 +0900

    feat: 접근제한자 삭제, 프로퍼티 타입 변환 16진수 변환 코드 작성
    - Rgb 클래스는 객체 오브젝트 이기 때문에 private 접근제한자 키워드가 불필요하다는 판단에 따라 삭제
    - rgb 값을 UIColor 변환에 사용하기 위해 기존의 Int 타입에서 CGFloat 타입으로 변환
    - RGB값을 16진수로 변환해주는 변수를 extension을 이용하여 구현

--- DrawingApp/DrawingApp/Random/RandomRgb.swift
+++ DrawingApp/DrawingApp/Random/RandomRgb.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 class Rgb {
-    private let red = Int.random(in: 0...255)
-    private let green = Int.random(in: 0...255)
-    private let blue = Int.random(in: 0...255)
+    let red = CGFloat.random(in: 0...255)
+    let green = CGFloat.random(in: 0...255)
+    let blue = CGFloat.random(in: 0...255)
 }
 
 extension Rgb: CustomStringConvertible {
     var description: String {
         return "R:\(red), G:\(green), B:\(blue)"
+    }
+}
+
+extension Rgb {
+    var hexColor: String {
+        let hexRed = String(format: "%02X", Int(red))
+        let hexGreen = String(format: "%02X", Int(green))
+        let hexBlue = String(format: "%02X", Int(blue))
+        return "\(hexRed)\(hexGreen)\(hexBlue)"
     }
 }
