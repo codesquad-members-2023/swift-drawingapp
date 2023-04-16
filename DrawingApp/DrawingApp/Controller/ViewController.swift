f5b0fdc42d334bc30efbd22e3f09602484b693e2
demiiann <tkim0156@gmail.com>
2023-04-16 21:52:35 +0900

    feat: ViewController 프로젝트 요구사항에 맞게 구현
    - 필요한 내부 속성 선언 및 할당
    - 필요한 view들의 IBOutlet 연결
    - 기능에 필요한 메서드 구현
    1. setStepper = 사각형 UIView의 투명도를 조절해주는 메서드
    최소/최대값 및 단위 변화값 설정
    2. setControlPanel = 화면의 우측 화면을 조건에 따라 숨기고 나타내게 하는 메서드
    3. alphaValueChanged = 투명도 값을 바꾸는 메서드, changeRgba와 매핑되어 있음
    4. setBackgroundColorTapGesture = 우측 화면에서 16진수 코드 부분의 label을 터치했을때 실행되는 메서드, tappedBackgroundColor를 실행한다
    5. tappedBackgroundColor = 배경색 변경을 실행하는 메서드, changeRgba와 매핑 되어 있음
    6. changeRgba = index와 alpha값을 매개변수로 받고, 해당 값을 이용하여 UIColor 타입을 받아온 후 UIView의 배경색, 투명도를 변경함. 그 이후 selectedView에 바뀐 UIView를 재등록하고, setBackgroundText 메서드를 실행
    7. planeAreaDidTouched = planeArea를 사용자가 터치했을때 실행되는 메서드. 터치 부분의 x,y좌표를 추출하여 해당 좌표에 사각형이 있는지 없는지 판단하고 있으면 사각형의 테두리 두께, 색을 변경하고 우측 패널을 활성화 시킨 후에 사각형의 배경색, 투명도에 대한 정보를 반영한다. 없으면 이전에 터치되었던 사각형의 테두리를 원래대로 바꾸고 우측 패널을 비활성화한다.
    8. setBackgroundText = 우측 패널에 배경색 16진수 코드 및 투명도 정보를 수정해주는 메서드
    9. makeRectangle = 사각형을 생성하는 메서드. 해당 메서드에서 모델과 뷰를 생성하고 동시에 각자의 배열에 추가하여 동일한 index를 갖게 한다.

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -6,25 +6,100 @@
 //
 
 import UIKit
-import OSLog
 
 class ViewController: UIViewController {
-    private let numberOfRectangles = 4
-
+    private var uiViewIndex = 0
+    private var plane = Plane()
+    private var rectangleFactory = RectangleFactory()
+    private var selectedView: UIView?
+    private var rectangleViews = [UIView]()
+    
+    @IBOutlet weak var planeArea: UIView!
+    @IBOutlet weak var backgroundColorHexCode: UILabel!
+    @IBOutlet weak var alphaStepper: UIStepper!
+    @IBOutlet weak var controlPanel: UIStackView!
+    @IBOutlet weak var alphaText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        generateRandomRectangles(numberOfRectangles: self.numberOfRectangles)
+        setControlPanel(hidden: true)
+        setBackgroundColorTapGesture()
+        setStepper()
     }
     
-    func generateRandomRectangles(numberOfRectangles: Int) {
-        let widthOfView = view.frame.width
-        let heightOfView = view.frame.height
-        let rectangleFactory = RectangleFactory()
+    func setStepper() {
+        alphaStepper.maximumValue = 10
+        alphaStepper.minimumValue = 1
+        alphaStepper.stepValue = 1
+    }
+
+    func setControlPanel(hidden: Bool) {
+        if hidden { controlPanel.isHidden = true }
+        else { controlPanel.isHidden = false }
+    }
+    
+    @IBAction func alphaValueChanged(_ sender: UIStepper) {
+        changeRgba(index: uiViewIndex, alpha: sender.value)
+    }
+    
+    func setBackgroundColorTapGesture() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedBackgroundColor))
+        backgroundColorHexCode.isUserInteractionEnabled = true
+        backgroundColorHexCode.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func tappedBackgroundColor(sender: UITapGestureRecognizer) {
+        changeRgba(index: uiViewIndex)
+    }
+    
+    func changeRgba(index: Int, alpha: Double = -1) {
+        var rgba: UIColor
         
-        for i in 1...numberOfRectangles {
-            let rectangle = rectangleFactory.makeRectangle(viewWidth: widthOfView, viewHeight: heightOfView)
-            os_log("Rect\(i) \(rectangle)")
+        if alpha == -1 {
+            rgba = plane.changeBackgroundColor(index: uiViewIndex)
+        } else {
+            rgba = plane.changeAlpha(index: uiViewIndex, alpha: alpha)
+        }
+        
+        rectangleViews[uiViewIndex].backgroundColor = rgba
+        selectedView = rectangleViews[uiViewIndex]
+        setBackgroundText(index: uiViewIndex)
+    }
+    
+    @IBAction func planeAreaDidTouched(_ sender: UITapGestureRecognizer) {
+        selectedView?.layer.borderWidth = 0
+        
+        let location = sender.location(in: planeArea)
+        let x = location.x
+        let y = location.y
+        uiViewIndex = plane.getUiViewIndex(x: x, y: y)
+        
+        if uiViewIndex == -1 {
+            setControlPanel(hidden: true)
+            selectedView = nil
+            return
+        } else {
+            let rectangleView = rectangleViews[uiViewIndex]
+            setControlPanel(hidden: false)
+            selectedView = rectangleView
+            selectedView?.layer.borderWidth = 4
+            selectedView?.layer.borderColor = UIColor.black.cgColor
+            setBackgroundText(index: uiViewIndex)
         }
     }
-}
+    
+    func setBackgroundText(index: Int) {
+        guard let selectedRectangle = plane[index] else { return }
+        backgroundColorHexCode.text = "#\(selectedRectangle.color.hexColor)"
+        alphaText.text = "투명도 : \(selectedRectangle.alpha)"
+        alphaStepper.value = selectedRectangle.alpha.alpha
+    }
 
+    @IBAction func makeRectangle(_ sender: Any) {
+        let newRectangle = rectangleFactory.makeRectangle(viewWidth: planeArea.frame.width, viewHeight: planeArea.frame.height)
+        let rectView = plane.makeUiView(rect: newRectangle)
+        plane.addRectangle(rectangle: newRectangle)
+        rectangleViews.append(rectView)
+        planeArea.addSubview(rectView)
+    }
+}
